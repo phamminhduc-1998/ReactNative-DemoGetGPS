7dae5304c84fee2dde3b8d7526074bcbdb54ea34
phamminhduc-1998 <56750938+phamminhduc-1998@users.noreply.github.com>
2020-11-13 01:26:35 +0700

    Demo Get GPS Location

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
+
+
+{/*
+Installation
+expo install expo-location  cho phép đọc thông tin định vị từ thiết bị
+ */}
 
 export default function App() {
+  const [longintude, setLongintude] = useState('longintude');//Kinh độ
+  const [latitude, setLatitude] = useState('latitude'); //Vỹ độ
+
+
+  let getGPS = () => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      // setLocation(location);
+      setLatitude(location.coords.latitude);
+      setLongintude(location.coords.longitude);
+    })();
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vỹ độ : {latitude} : Kinh độ : {longintude}</Text>
+      <Button title="Get Location" onPress={() => {
+        getGPS();
+      }} />
+
       <StatusBar style="auto" />
     </View>
   );
